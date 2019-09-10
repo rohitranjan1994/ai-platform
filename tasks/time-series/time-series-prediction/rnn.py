edd4bf4e6fe28130e2ababcec85016003993d142
Rohit Ranjan <rohitranjan1994@gmail.com>
2019-09-10 13:51:22 +0800

    upadted dropout and regulizer

--- tasks/time-series/time-series-prediction/rnn.py
+++ tasks/time-series/time-series-prediction/rnn.py
@@ -8,8 +8,8 @@ from keras.models import model_from_json
 from itertools import product
 from functools import partial, update_wrapper
 import time
-
 from keras import backend as K
+from keras.regularizers import L1L2
 
 
 class RNN:
@@ -23,11 +23,12 @@ class RNN:
 
         model = Sequential()
         model.add(LSTM(64, return_sequences=True,
-                       input_shape=(self.input_length, self.input_dim)))
+                       input_shape=(self.input_length, self.input_dim),bias_regularizer=L1L2(l1=0.01, l2=0.01)))
         model.add(LSTM(64, return_sequences=False, input_shape=(self.input_length, self.input_dim)))
         model.add(Dropout(0.5))
         model.add(Dense(32, activation='tanh'))
         model.add(Dense(self.output_dim, activation='softmax'))
+        #model.add(bias_regularizer=L1L2(l1=0.01, l2=0.01))
 
         print('Compile model.   ..')
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -42,7 +43,7 @@ def main():
     timestep = 50
     n_cross_validation = 3
     # for order book info only
-    data = data_prep.get_test_data(timestep, predict_step=5, filename="order_book_random.csv")
+    data = data_prep.get_test_data(timestep, predict_step=5, filename="./data/order_book_sample.csv")
 
     # input_shape <- (timestep, n_features)
     # output_shape <- n_classes
@@ -56,6 +57,8 @@ def main():
 
     # print("TRAIN WITH CROSS-VALIDATION")
     # nn.run_with_cross_validation(data, n_cross_validation)
+
+
 
 if __name__ == '__main__':
     main()
